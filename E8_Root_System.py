3321f29732578961d24f6e4d42204706118353a9
wyfly87 <zhao_liang@pku.edu.cn>
2016-05-24 18:50:05 +0800

    Update E8_Root_System.py

--- E8_Root_System.py
+++ E8_Root_System.py
@@ -44,5 +44,54 @@ for i in range(240):
             
 #--- Step One Finished ---#
 
+#--- Step Two: compute a basis of the Coxeter plane ---#
+
+# Firstly let's write down explicitly a complete set of simple roots.
+# Any choice of the set of simple roots would surfice.
+# In the following the simple roots are given by the rows of the matrix "delta"
+delta = np.eye(8)
+for i in range(5):
+    delta[i,i+1] = -1
+delta[5,6] = 1
+delta[6] = -0.5
+delta[7,5] = 1
+delta[7,6] = -1
+delta[7,7] = 0
+
+# Then the Cartan matrix will be:
+cartan = np.dot(delta,delta.transpose())
+
+# The Dynkin graph:
+# 1---2---3---4---5---6---7
+#                 |
+#                 8
+# Where label i corresponding to the i-th row of delta.
+
+# Now we split the simple roots into two disjoint sets I and J
+# such that the simple roots in I are pairwise orthogonal (as well as the roots in J).
+# It's obvious to see how to find such a splitting given the Dynkin graph above:
+# I = [1,3,5,7] and J = [2,4,6,8]
+# since roots are not connected by an edge if and only if they are orthogonal.
+# Then a basis of the Coxeter plane is given by
+# u = sum (c[i] * delta[i]) for i in I
+# v = sum (c[j] * delta[j]) for j in J
+# Where c is the eigenvector corresponding to the maximal eigenvalue of the Cartan matrix.
+
+# The eigenvalues returned by eigh() are in ascending order and the eigenvectors are listed by columns. 
+I = [0,2,4,6]
+J = [1,3,5,7]
+eigenvals,eigenvecs= np.linalg.eigh(cartan)
+c = eigenvecs[:,7]
+u = np.sum([ c[i]*delta[i] for i in I ],axis=0)
+v = np.sum([ c[j]*delta[j] for j in J ],axis=0)
+
+# Gram-Schimdt and normalize to unit vectors.
+u /= np.linalg.norm(u)
+v = v - np.dot(u,v)*u
+v /= np.linalg.norm(v)
+
+#--- Step Two Finished ---#
 
 
+
+   

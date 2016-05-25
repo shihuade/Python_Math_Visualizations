9009ac53c76e6420602b7687cd94541472af35c3
wyfly87 <zhao_liang@pku.edu.cn>
2016-05-25 18:40:05 +0800

    Update Mandelbrot_Smooth_Coloring.py

--- Mandelbrot_Smooth_Coloring.py
+++ Mandelbrot_Smooth_Coloring.py
@@ -1,1 +1,15 @@
 #coding = utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+max_iterations = 500
+radius = 50
+figsize = (1200, 960)
+
+def escape(c):
+    z = 0
+    for i in xrange(max_iterations):
+        if abs(z) > radius:
+            break
+        z = z**2 + c

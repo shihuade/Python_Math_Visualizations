d086f6e8c2ab86f901e35c790c0713573daba0f4
wyfly87 <zhao_liang@pku.edu.cn>
2016-05-27 11:24:16 +0800

    Update Pencil_Draw.py

--- Pencil_Draw.py
+++ Pencil_Draw.py
@@ -6,11 +6,23 @@ import scipy.sparse as sps
 from scipy.misc import imrotate
 from scipy.signal import convolve2d as conv2
 
+# The Input_Image is the image that to be processed,
+# The Pencil_Texture is a suitably chosen texture image that determine the style of the output.
 Input_Image = "house.jpg"
 Pencil_Texture = "pencil01.jpg"
 
+# Now the global parameters:
+Line_Len = 19  # must be an odd
+kr = Line_Len // 2  # kernel radius
+dirnum = 8
+Lambda = 0.2
+
+
+# The following two functions make conversions between image instances and numpy arrays.
 def img_to_np(img):
     return np.array(img).astype(np.float)/255
 
 def np_to_img(I):
     return im.fromarray((255*I).round().astype(np.uint8))
+    
+    

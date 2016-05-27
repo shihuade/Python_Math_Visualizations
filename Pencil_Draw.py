c0be6aad902b3173cc7ca39e56f7ca6de6ef12f7
wyfly87 <zhao_liang@pku.edu.cn>
2016-05-27 09:04:39 +0800

    Update Pencil_Draw.py

--- Pencil_Draw.py
+++ Pencil_Draw.py
@@ -3,3 +3,14 @@
 import numpy as np
 import Image as im
 import scipy.sparse as sps
+from scipy.misc import imrotate
+from scipy.signal import convolve2d as conv2
+
+Input_Image = "house.jpg"
+Pencil_Texture = "pencil01.jpg"
+
+def img_to_np(img):
+    return np.array(img).astype(np.float)/255
+
+def np_to_img(I):
+    return im.fromarray((255*I).round().astype(np.uint8))

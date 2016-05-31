657f9b300dcc5ebc45c47981abcc78f83e004771
wyfly87 <zhao_liang@pku.edu.cn>
2016-05-31 16:27:20 +0800

    Update Pencil_Draw.py

--- Pencil_Draw/Pencil_Draw.py
+++ Pencil_Draw/Pencil_Draw.py
@@ -32,3 +32,46 @@ def img_to_np(img):
 
 def np_to_img(I):
     return im.fromarray((255*I).round().astype(np.uint8))
+    
+def GetStroke(I):
+    """
+    The input I is a numpy 2d-array within [0,1]. 
+    The output S has the same shape and data type with I.
+    """
+    
+def GetTone(I, omega=0):
+    """
+    Perform histogram matching so that the distribution of the result image matches the 
+    empirical distributions learned from artist-drawn images.
+    The input I should be a numpy 2d-array within [0,1].
+    The output J has the same shape and dtype with I.
+    """
+    I = (255*I).round().astype(np.uint8)
+    counts = np.bincount(I.ravel(), minlength=256)
+    source_cdf = np.cumsum(counts).astype(np.float)
+    source_cdf /= source_cdf[-1]
+    
+    p = np.zeros(256)
+    for i in range(255):
+        p1 = np.exp( - (255-i) / 9.0 ) / 9
+        p2 = (i>=105 and i<=225) / (225.0-105.0)
+        p3 = np.exp( -(i-90)**2 / (2.0*121) ) / np.sqrt(2*np.pi*11) 
+        if omega == 0:
+            w = [76, 22, 2]
+        elif omega == 1:
+            w = [52, 37, 11]
+        elif omega == 2:
+            w = [42, 29, 29]
+        else:
+            raise ValueError("omega must take values in [0,1,2]")
+        p[i] = np.dot([p1,p2,p3], w)
+        
+    target_cdf = np.cumsum(p)
+    target_cdf /= target_cdf[-1]
+    
+    values, bins = np.unique(I.ravel(), return_inverse = True)
+    interp_values = np.interp(source_cdf, target_cdf, range(256))
+    J = interp_values[bins].reshape(I.shape)
+    J /= 255.0
+    return J
+    

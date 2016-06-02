dd08a5f5b0c05595c7e167d78a9747d86eb625c4
Wang Ying         王颖 <zhao_liang@pku.edu.cn>
2016-06-03 03:17:43 +0800

    Update Zombie_Simulation.py

--- Zombie_Outbreak_Simulation/Zombie_Simulation.py
+++ Zombie_Outbreak_Simulation/Zombie_Simulation.py
@@ -5,7 +5,19 @@ sys.setdefaultencoding("utf-8")
 
 import numpy as np
 import moviepy.editor as mpy
+from scipy.signal import convolve2d as conv2
 
-usa = mpy.ImageClip("usa_density.png")
+usa = mpy.ImageClip("usa_density.png").resize(width=600)
 SIR = np.zeros((3, usa.h, usa.w)).astype(np.float)
 SIR[0] = usa.get_frame(0) / 255
+
+dispersion_rates = [0, 0.5, 0.3]
+dispersion_kernel = np.array([[0.5, 1 , 0.5],
+                              [1, -6, 1],
+                              [0.5, 1, 0.5]]) / 6
+
+infection_rate = 0.5
+incubation_rate = 0.8
+
+def Infection(SIR, infection_rate, incubation_rate):
+  

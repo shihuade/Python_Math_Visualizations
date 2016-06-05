a603c1b3b5518e4b338c31e1f60167398e0aff86
Wang Ying         王颖 <zhao_liang@pku.edu.cn>
2016-06-05 22:06:36 +0800

    Update Lorenz_Attractor.py

--- Lorenz_Attractor.py
+++ Lorenz_Attractor.py
@@ -13,11 +13,14 @@ N = 20
 sigma, beta, rho = 10.0, 8/3.0, 28.0
 
 def derivative((x,y,z),t0):
-    """返回点 (x,y,z) 处的切方向"""
+    """
+    返回点 (x,y,z) 处的切方向
+    """
     return sigma*(y-x), x*(rho-z)-y, x*y-beta*z
 
 def solution(x_0,t):
-    """对初始位置 x_0 和时间数组 t, 返回解在 t 的每个元素处的值
+    """
+    对初始位置 x_0 和时间数组 t, 返回解在 t 的每个元素处的值.
     sicpy.odeint() 函数接受一个函数 f, 一个初始值 x_0 和一个列表 t 为参数, 返回值是 t 对应的解组成的列表.
     f 应当为　dy/dt = f(y,t) 返回解 y 在 t 时刻的导数值.
     """
@@ -27,11 +30,9 @@ x_0 = -15 + 30 * np.random.random((N,3)) # 随机生成 N 个点
 t = np.linspace(0,4,1001)
 x_t = np.array([ solution(z,t) for z in x_0])
 
-
 # 每个点 z 的轨迹是一条空间曲线 z(t), 对区间[0,4]中的 t 计算 z(t), 把这些点连起来就得到了轨迹.
 # >>> x_t.shape
 # >>> (20,1000,3)
-
 
 fig = plt.figure(figsize=(8,8))
 ax = fig.gca(projection='3d',xlim=(-25,25), ylim=(-35,35),zlim=(5,55))

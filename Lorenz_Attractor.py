87d94bd8c89d2aae806e55f1016b761144f718a6
Wang Ying         王颖 <zhao_liang@pku.edu.cn>
2016-06-05 21:59:44 +0800

    Create Lorenz_Attractor.py

---
+++ Lorenz_Attractor.py
@@ -0,0 +1,7 @@
+#coding = utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+from scipy.integrate import odeint  
+from mpl_toolkits.mplot3d import Axes3D  

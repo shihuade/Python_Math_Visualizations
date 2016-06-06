d13aea7ec73e43757bce88b25654744c164427c2
Wang Ying         王颖 <zhao_liang@pku.edu.cn>
2016-06-07 04:54:26 +0800

    Update Wilson_Uniform_Spanning_Tree.py

--- Wilson_Uniform_Spanning_Tree.py
+++ Wilson_Uniform_Spanning_Tree.py
@@ -1,3 +1,39 @@
 # coding = utf-8
 
 import random
+
+
+def grid(*size):
+    
+    def neighbors(v):
+        neighborhood = []
+        for i in range(len(size)):
+            for dx in [-1,1]:
+                w = list(v)
+                w[i] += dx
+                if 0 <= w[i] < size[i]:
+                    neighborhood.append(tuple(w))
+        return neighborhood
+        
+    return {v: neighbors(v) for v in product(*map(range, size))}
+    
+
+def ust(graph):
+    
+    root = random.choice(graph.keys())     
+    parent = {root: None}
+    tree = set([root])
+    
+    for vertex in graph:
+        v = vertex
+        while v not in tree:
+            neighbor = random.choice(graph[v])
+            parent[v] = neighbor
+            v = neighbor
+
+        v = vertex
+        while v not in tree:
+            tree.add(v)
+            v = parent[v]
+    return parent
+    

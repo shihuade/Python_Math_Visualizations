53d77809d46657711d6d622d9f1eb9e16e48556d
Wang Ying         王颖 <zhao_liang@pku.edu.cn>
2016-06-08 19:26:05 +0800

    Create Icosa_Symmetry.py

---
+++ Matplotlib_Gallery/Icosa_Symmetry.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import hsv_to_rgb
+
+def Klein(z):
+    #Klein j-function
+    return 1728*(z*(z**10+11*z**5-1))**5 / (-(z**20+1)+228*(z**15-z**5)-494*z**10)**3
+
+def RiemannSphere(z):
+    #mapping to Riemann Sphere via stereographic projection
+    t = 1 + z.real**2 + z.imag**2
+    return 2*z.real/t, 2*z.imag/t, 2/t-1
+
+def Mobius(z):
+    #distort the result image by a mobius transformation
+    return (z-20)/(3*z+1j)
+
+x,y = np.ogrid[-5:5:800j,-5:5:800j]
+z = x + y*1j
+z = RiemannSphere(Klein(Mobius(Klein(z))))
+
+H = np.sin(z[0]*np.pi)**2
+S = np.cos(z[1]*np.pi)**2
+V = abs(np.sin(z[2]*np.pi) * np.cos(z[2]*np.pi))**0.2
+
+HSV = np.dstack((H,S,V))
+RGBImage = hsv_to_rgb(HSV)
+
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_axes([0,0,1,1], aspect=1)
+ax.axis('off')
+plt.imshow(RGBImage)
+#plt.show()
+plt.savefig('Icosa_Symmetry.png')
